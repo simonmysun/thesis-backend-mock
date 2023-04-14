0487ed427e8612b08d60970e9997fec4c82e3b46
Maoyin <simonmysun@gmail.com>
2023-04-14 15:05:12 +0200

    do not try to publish when disconnected

--- mqtt.js
+++ mqtt.js
@@ -12,75 +12,101 @@ const msgFreq = 1500; // interval ms
 const deviceID = 'fake_datasource';
 
 const options = {
-  keepalive: 60,
-  clientId: clientId,
-  protocolId: 'MQTT',
-  protocolVersion: 4,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  username: username,
-  password: password,
-  will: {
-    topic: 'WillMsg',
-    payload: 'Connection Closed abnormally..!',
-    qos: 0,
-    retain: false
-  },
+    keepalive: 60,
+    clientId: clientId,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    username: username,
+    password: password,
+    will: {
+        topic: 'WillMsg',
+        payload: 'Connection Closed abnormally..!',
+        qos: 0,
+        retain: false
+    },
 };
 
 const noise2D = createNoise2D();
 
 function hashFnv32a(str, asString, seed) {
-  let i;
-  let l;
-  let hval = (seed === undefined) ? 0x811c9dc5 : seed;
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-  }
-  if(asString){
-    // Convert to 8 digit hex string
-    return ("0000000" + (hval >>> 0).toString(16)).slice(-8);
-  }
-  return hval >>> 0;
+    let i;
+    let l;
+    let hval = (seed === undefined) ? 0x811c9dc5 : seed;
+    for (i = 0, l = str.length; i < l; i++) {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    if(asString){
+        // Convert to 8 digit hex string
+        return ("0000000" + (hval >>> 0).toString(16)).slice(-8);
+    }
+    return hval >>> 0;
 }
 
 console.log(`Connecting to ${username}@${host} ...`);
 const client = mqtt.connect(host, options);
 
+let connected = false;
+
 client.on('error', (err) => {
-  console.log('Connection error: ', err);
-  client.end();
+    console.log('Connection error: ', err);
+    connected = false;
+    client.end();
 })
 
 client.on('reconnect', () => {
-  console.log('Reconnecting...');
+    connected = true;
+    console.log('Reconnecting...');
+});
+
+client.on('close', () => {
+    connected = false;
+    console.log('Conneciton closed...');
+});
+
+client.on('offline', () => {
+    connected = false;
+    console.log('Offline...');
+});
+
+client.on('disconnect', () => {
+    connected = false;
+    console.log('Disconnected...');
+});
+
+client.on('end', () => {
+    connected = false;
+    console.log('Connection ended...');
 });
 
 var intervalHandler = -1;
 
 client.on('connect', function () {
-  console.log('Connection established');
-  clearInterval(intervalHandler);
-  intervalHandler = setInterval(((client, length) => (_ => {
-    const arr = Array.from({ length: length }).map((x, i) => {
-      let noise = noise2D(hashFnv32a(deviceID) % 1024 + i / length * 2, new Date().valueOf() % 1000000 / 10000);
-      noise = noise ** 2;
-      noise = noise < 0.5 ? 8 * noise * noise * noise * noise : 1 - Math.pow(-2 * noise + 2, 4) / 2;
-      return noise;
-    });
-    client.publish('update:' + deviceID, JSON.stringify({
-      timestamp: Date.now(),
-      payload: arr
-    }));
-    console.log('update');
-  }))(client, hashFnv32a(deviceID) % 16 + 16), msgFreq);
-})
+    console.log('Connection established');
+    connected = true;
+    clearInterval(intervalHandler);
+    intervalHandler = setInterval(((client, length) => (_ => {
+        if(connected) {
+            const arr = Array.from({ length: length }).map((x, i) => {
+                let noise = noise2D(hashFnv32a(deviceID) % 1024 + i / length * 2, new Date().valueOf() % 1000000 / 10000);
+                noise = noise ** 2;
+                noise = noise < 0.5 ? 8 * noise * noise * noise * noise : 1 - Math.pow(-2 * noise + 2, 4) / 2;
+                return noise;
+            });
+            client.publish('update:' + deviceID, JSON.stringify({
+                timestamp: Date.now(),
+                payload: arr
+            }));
+            console.log('update');
+        }
+    }))(client, hashFnv32a(deviceID) % 16 + 16), msgFreq);
+});
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
-})
-
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+});
